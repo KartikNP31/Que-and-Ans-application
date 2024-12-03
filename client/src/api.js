17a41b3cd79e1778378ba0f4c3571339de15682a
KartikNP31 <patelkn1303@gmail.com>
2024-12-03 18:57:30 +0530

    Update PUBLIC_SERVER_URL to remove trailing slash

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
 // export const PUBLIC_SERVER_URL="http://localhost:8000"
-export const PUBLIC_SERVER_URL="https://que-and-ans-application-3x45.vercel.app/"
+export const PUBLIC_SERVER_URL="https://que-and-ans-application-3x45.vercel.app"
 // export const PUBLIC_SERVER_URL="http://localhost:8000"
 // 
