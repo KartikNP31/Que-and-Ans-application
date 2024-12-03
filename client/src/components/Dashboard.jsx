54b855db90fda37ebd16f794a241911b2412d45b
KartikNP31 <patelkn1303@gmail.com>
2024-12-04 01:16:43 +0530

    Redirect unauthenticated users to WelcomePage; implement useAuth and useEffect in Dashboard component

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Sidebar";
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
 
 export const Dashboard = () => {
+  const { isSignedIn } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isSignedIn) {
+      navigate("/WelcomePage");
+    }
+  }, [isSignedIn, navigate]);
   return (
     <div className="flex h-[800px]">
       <Sidebar />
